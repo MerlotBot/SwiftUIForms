1f7aa563aa0a7f95d01640ddbabc407a8a3ffc0b
Nikolaos Antonakakis <n.antonakakis@gmail.com>
2019-07-22 14:16:51 +0200

    implemented simple form with slider, toggle and button

--- Forms/ContentView.swift
+++ Forms/ContentView.swift
@@ -9,8 +9,59 @@
 import SwiftUI
 
 struct ContentView : View {
+    @State private var scheduled: Bool =  false
+    @State private var manuallyEnableUntilTomorrow: Bool = false
+    @State private var colorTemperature: CGFloat = 0.5
+    
     var body: some View {
-        Text("Hello World")
+        
+        NavigationView {
+            
+            Form {
+                
+                Section(header: Text("Night shift automatically colors during the night so as to protect your eyesight")
+                    .lineLimit(nil)
+                    .padding(5)) {
+                        Toggle(isOn: $scheduled) {
+                            Text("Scheduled")
+                        }
+                        HStack {
+                            
+                            VStack(alignment: .leading) {
+                                Text("From")
+                                Text("To")
+                            }
+                            Spacer()
+                            NavigationLink(destination: Text("Scheduled Settings")) {
+                                
+                                VStack {
+                                    Text("Sunset").color(.blue)
+                                    Text("Sunrise").color(.blue)
+                                }
+                            }.fixedSize()
+                        }
+                }
+                
+                Section(header: Text("").padding()) {
+                    Toggle(isOn: $manuallyEnableUntilTomorrow) {
+                        Text("Manually enable until tomorrow")
+                    }
+                }
+                
+                Section(header: Text("COLOR TEMPERATURE").padding([.top, .bottom], 7)) {
+                    
+                    VStack {
+                        HStack {
+                            Text("Less Warm")
+                            Spacer()
+                            Text("More Warm")
+                        }
+                        Slider(value: $colorTemperature, from: 0.0, through: 1.0, by: 0.1)
+                    }
+                }
+                
+            }.navigationBarTitle(Text("Settings"))
+        }
     }
 }
 
